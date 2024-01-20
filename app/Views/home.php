f69e6716b738be2915e71e89200e415df1481a1e
deviliaan <blankblankboo@gmail.com>
2024-01-20 08:10:29 +0000

    db added

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,7 +1,17 @@
+<?php 
+  $db = \Config\Database::connect();
+?>
+
 <?= $this->extend('layouts/default') ?>
 <?= $this->section('content') ?>
 <div class="container bg-slate-800">
     <div class="">
+      <?php
+        $query = $db->query('SELECT title FROM animes');
+        $result = $query->getResult();
+        echo '<pre>';
+        print_r($result);
+      ?>
       wellcome to codeigniter
     </div>
 </div>

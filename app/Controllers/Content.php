ab5608e5d4412af8613b28077eb1492a658f7c12
kai00588 <blankblankboo@gmail.com>
2024-01-23 17:55:52 +0530

    added info

--- app/Controllers/Content.php
+++ app/Controllers/Content.php
@@ -3,12 +3,15 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-
+use App\Models\AnimeModel;
 class Content extends BaseController
 {
     
     public function single($id)
     {
-        return view('content',['id'=>$id]);
+        $provider = new AnimeModel();
+        $anime = $provider->where('id',$id)->find();
+        $anime = json_decode(json_encode($anime[0],FALSE));
+        return view('content',['anime'=>$anime]);
     }
 }
--- app/Views/content.php
+++ app/Views/content.php
@@ -1,6 +1,18 @@
 <?= $this->extend('layouts/default');?>
 <?= $this->section('content');?>
-<div>
-    <h2>Content <?= $id?></h2>
+<div class="mt-2">
+    <div class="grid grid-cols-2 p-2">
+        <div class="p-2">
+            <img src="<?= $anime->image?>" alt="<?= $anime->title?>" srcset="">
+        </div>
+        <div>
+            <h2><?= $anime->title?></h2>
+            <h1>Episode: <?= $anime->latest_episode?></h1>
+            <span>Type: <?= $anime->type?></span>
+        </div>
+    </div>
+    <div class="p-4">
+        <?= $anime->story?>
+    </div>
 </div>
 <?= $this->endSection()?>
